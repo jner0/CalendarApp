ac9f90a8522f87f990241cf33771fec215f65b3a
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-07-18 22:07:05 -0500

    End of the app

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,16 +1,18 @@
-import { BrowserRouter } from "react-router-dom"
-import { AppRouter } from "./router"
-import { Provider } from "react-redux"
-import { store } from "./store"
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import { AppRouter } from "./router";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export const CalendarApp = () => {
   return (
     <>
       <Provider store={store}>
+        {/* <HashRouter>
+        </HashRouter> */}
         <BrowserRouter>
-          <AppRouter/>
+          <AppRouter />
         </BrowserRouter>
       </Provider>
     </>
-  )
-}
+  );
+};
